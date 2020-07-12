e3770993afa2ea0c3c49ae5799e0459e9648a83c
chanakya <jini1310@gmail.com>
2020-07-12 11:53:10 +0530

    Added requirements.txt and done some analysis

--- parse.py
+++ parse.py
@@ -34,4 +34,25 @@ df = pd.DataFrame(parsedData, columns = ['Date', 'Time', 'Author', 'Message'])
 df['Date'] = pd.to_datetime(df['Date'])
 print(df.head(5))
 df = df.dropna()
-df.to_csv('Whatsapp-Chats.csv') 
+df.to_csv('Whatsapp-Chats.csv')
+
+# Performing Some Analysis on the data.
+
+print(df.head(5))
+a = dict(df['Author'].value_counts())
+
+names = np.array(list(a.keys()))
+print("The number of people in the group are {} and their names are {}".format(len(names), names))
+
+data = pd.Series(a).reset_index(name='value').rename(columns={'index':'names'})
+data['angle'] = data['value']/data['value'].sum() * 2 * math.pi
+data['color'] = Category20c[len(a)]
+
+p = figure(plot_height = 350, title = "Pie Chart", toolbar_location = None,
+        tools = "hover", tooltips = "@names: @value")
+
+p.wedge(x = 0, y = 1, radius = 0.4,
+        start_angle = cumsum('angle', include_zero=True), end_angle = cumsum('angle'),
+        line_color = "white", fill_color = 'color', legend = 'names', source = data)
+
+show(p)
